3a488ba5c2d0cf55110066a761eb81bbf55379d1
Suhe1l <astudiobyad@gmail.com>
2024-10-07 14:12:48 +0300

    feat:Add Navbar
     Added and styled navbar with empty links as I continue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,15 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <section>
-      <Link to='/' className="font-tacOne text-3xl">
-        FITrack
-      </Link>
+      <div className="flex justify-between m-auto shadow-md">
+        <Link to='/' className="text-center content-center font-tacOne text-3xl m-6 hover:scale-110 transition duration-500">FITrack</Link>
+        <div className="m-6 flex flex-row items-center">
+          <Link to='/' className="nav-link mx-10" >Home</Link>
+          <Link to='#about' className="nav-link mx-10">About</Link>
+          <Link to='#contact' className="nav-link mx-10">Contact</Link>
+          <Link className="btn-black mx-8">Get Started</Link>
+        </div>
+      </div>
     </section>
   );
 }
