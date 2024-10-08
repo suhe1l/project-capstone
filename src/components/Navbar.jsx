a610b5ec1190236b1f2782aa015cf30fadfac3f9
Suhe1l <astudiobyad@gmail.com>
2024-10-08 07:40:15 +0300

    fix:change navbar to include routing
     Changed navigation from using anchor tags to routing as user could not navigate back to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,18 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="fixed top-0 w-full bg-white py-4 z-20 shadow-md">
-      <div className="container mx-auto flex justify-between items-center px-6">
-        <div className="text-3xl font-tacOne">FITrack</div>
-        <ul className="hidden md:flex space-x-12 text-black font-teko text-xl">
-          <li><a href="#home" className="hover:text-green-500">Home</a></li>
-          <li><a href="#about" className="hover:text-green-500">About Us</a></li>
-          <li><a href="#services" className="hover:text-green-500">Services</a></li>
-        </ul>
-        <div className="hidden md:flex">
-          <Link to="/app" className="btn-black">
-            Get Started
-          </Link>
+        <div className="container mx-auto flex justify-between items-center px-6">
+            <Link to="/" className="text-3xl font-tacOne">FITrack</Link>
+            <ul className="hidden md:flex space-x-12 text-black font-teko text-xl">
+                <li><Link to="/" className="hover:text-green-500">Home</Link></li>
+                <li><Link to="/app" className="hover:text-green-500">App</Link></li>
+            </ul>
+            <div className="hidden md:flex">
+                <Link to="/app" className="btn-black">
+                    Get Started
+                </Link>
+            </div>
         </div>
-      </div>
     </nav>
   );
 }
