8cba63abb3e90d4a1d31dde8904c4cc96f46be91
Suhe1l <astudiobyad@gmail.com>
2024-10-08 08:01:02 +0300

    fix:remove search component from HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
-import Exercises from '../components/Exercises';
-import SearchExercises from '../components/SearchExercises';
 import HeroSection from '../components/HeroSection';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
-      <SearchExercises />
-      <Exercises />
     </div>
   )
 }
