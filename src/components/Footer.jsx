25f402cd9229c82bb1c222ebf5b1a775dbf8cc90
Suhe1l <astudiobyad@gmail.com>
2024-10-12 12:18:26 +0300

    fix:add responsive footer
     Alignment of footer on mobile fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="bg-black w-full pt-10 px-10">
         <div className="flex flex-col md:flex-row items-center lg:flex-row md:justify-between lg:justify-between m-auto mb-10 px-10">
           <Link to="/" className="text-4xl text-white md:text-6xl lg:text-7xl font-tacOne hover:text-green-500">FITrack</Link>
-          <ul className="flex flex-row mt-2 md:flex-col lg:flex-col lg:content-center lg-mr-10">
+          <ul className="flex flex-col items-center md:items-end mt-2 lg:content-center lg-mr-10">
             <li><Link to="/" className="nav-link flex text-2xl text-white">Home</Link></li>
             <li><Link to="/app" className="nav-link flex text-2xl text-white">App</Link></li>
           </ul>
